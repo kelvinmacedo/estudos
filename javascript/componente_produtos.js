034ce36c5be2151aaf6af9d181cb0f04eee0dbd6
KelvinMarquesMax <alex.macedo@showtecnologia.com>
2024-02-05 23:18:35 -0300

    feat (COD-01): testando o funcionamento da inserção dinamica das imagem aos produtos.

--- javascript/componente_produtos.js
+++ javascript/componente_produtos.js
@@ -2,28 +2,36 @@
 const itens = [
     {    
         id:2,
-        nome:"controle",
-        img:1234,
+        nome:"controle Xbox One",
+        img:'../image/controle-xbox-one-preto.jpeg',
         quantidade:1
     },
     {
         id:21,
-        nome:"gameBoy",
-        img:456,
+        nome:"gameBoy Advance",
+        img:'../image/game-boy-advance-03.jpg',
         quantidade:1
     },
     {
         id:4,
-        nome:"pião",
-        img:321,
+        nome:"Xbox 1ª Geração",
+        img:'../image/xbox-classico-1-geracao.jpeg',
         quantidade:1
     }
 ]
 
 renderizarProdutos = () => {
-    var conteinerProdutos = document.getElementById('produtos');
+    let conteinerProdutos = document.getElementById('produtos');
     itens.map((item)=>{
-        console.log(item.nome);
+        let itemProduto = document.createElement('div');
+        itemProduto.innerHTML += `
+            <img src=${item.img} alt="${item.nome} Image" class="product-image" style="width: 200px; heigth: 200px; padding: 3px;">
+            <h2 class="product-name">${item.nome}</h2>
+            <span class="product-quantity">Quantidade: ${item.quantidade}</span>
+        `;
+
+        conteinerProdutos.appendChild(itemProduto);
+
     });
 }
 
