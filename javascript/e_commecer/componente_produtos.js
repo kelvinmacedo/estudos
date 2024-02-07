6b2f6dd2b6b728754b40f8f01396c8a0020846a7
KelvinMarquesMax <alex.macedo@showtecnologia.com>
2024-02-06 23:05:01 -0300

    feat (COD-01): W.I.P ajustando layout dos produtos .

--- javascript/e_commecer/componente_produtos.js
+++ javascript/e_commecer/componente_produtos.js
@@ -6,23 +6,31 @@ const itens = [
         quantidade:1
     },
     {
-        id:21,
-        nome:"gameBoy Advance",
-        img:'../../midias/e_commecer/game-boy-advance-03.jpg',
-        quantidade:1
-    },
-    {
         id:4,
         nome:"Xbox 1ª Geração",
         img:'../../midias/e_commecer/xbox-classico-1-geracao.jpeg',
         quantidade:1
-    }
+    },
+    {    
+        id:3,
+        nome:"controle Xbox one S",
+        img:'../../midias/e_commecer/controle-xbox-one-preto.jpeg',
+        quantidade:5
+    },
+    {
+        id:8,
+        nome:"Xbox 1ª Geração full edição",
+        img:'../../midias/e_commecer/xbox-classico-1-geracao.jpeg',
+        quantidade:1
+    },
 ]
 
 renderizarProdutos = () => {
     let conteinerProdutos = document.getElementById('produtos');
+    
     itens.map((item)=>{
         let itemProduto = document.createElement('div');
+        itemProduto.classList.add('item-produto-class');
         itemProduto.innerHTML += `
             <img src=${item.img} alt="${item.nome} Image" class="product-image" style="width: 200px; heigth: 200px; padding: 3px;">
             <h2 class="product-name">${item.nome}</h2>
